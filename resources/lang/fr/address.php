a8624c632a021baebd8bbf94852e59e53ab7fec6
Jonathan SARDO <sardoj@gmail.com>
2019-01-06 15:16:05 +0100

    feat: Adds french locale files

---
+++ resources/lang/fr/address.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'address' => 'Adresses',
+    'address.single' => 'Adresse',
+    'block.address' => 'Adresse',
+    'field.address_1' => 'Adresse',
+    'field.address_2' => 'Adresse (suite)',
+    'field.postal_code' => 'Code postal',
+    'field.city' => 'Ville',
+    'field.country' => 'Pays',
+];
